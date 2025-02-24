5ab5888dcc655439eb01188d60764a818731bea5
ryanmiura <ryanmiuracarrasco@hotmail.com>
2025-02-24 01:33:01 -0300

    config docker

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,23 +14,29 @@ import (
 var client *mongo.Client
 
 func ConnectMongoDB() {
+	// Pegar a string de conexão das variáveis de ambiente
+	mongoURI := os.Getenv("MONGO_CONNECTION_STRING")
+	if mongoURI == "" {
+		log.Fatal("MONGO_CONNECTION_STRING não está definida")
+	}
 
-	url := "mongodb://localhost:27017"
-
-	clientOptions := options.Client().ApplyURI(url)
-
+	clientOptions := options.Client().ApplyURI(mongoURI)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
 	defer cancel()
 
 	var err error
 	client, err = mongo.Connect(ctx, clientOptions)
-
 	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		log.Fatalf("Falha ao conectar ao MongoDB: %v", err)
 	}
 
-	fmt.Println("Connected to MongoDB!")
+	// Ping no banco para verificar a conexão
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Falha ao pingar o MongoDB: %v", err)
+	}
+
+	fmt.Println("Conectado ao MongoDB!")
 }
 
 func GetMongoClient() *mongo.Client {
