11faf7b6a00f867b6a09d00d5d9ad7432d590c44
Micael <micaelrrocha13@gmail.com>
2025-04-02 16:46:01 -0300

    refactor: clean up CORS middleware configuration by removing redundant settings

--- backend/go/middlewares/cors.go
+++ backend/go/middlewares/cors.go
@@ -8,10 +8,6 @@ import (
 func CorsMiddleware() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 
-	config.AllowOrigins = []string{"http://localhost:3000"}                   // Permitir apenas a origem do frontend
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"} // Métodos permitidos
-	config.AllowHeaders = []string{"Authorization", "Content-Type"}           // Cabeçalhos permitidos
-
 	// Permitir credenciais (opcional se necessário)
 	config.AllowCredentials = true
 	config.AllowOrigins = []string{"http://localhost:3000"}                   // Permitir apenas a origem do frontend
