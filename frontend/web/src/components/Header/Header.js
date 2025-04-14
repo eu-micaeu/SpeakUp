c480da0eeba600c9133d07bafa85934c19e732e1
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2025-04-13 22:51:42 -0300

    feat: implement logout confirmation dialog in Header component and ad… (#30)
    * feat: implement logout confirmation dialog in Header component and adjust styles
    
    * feat: redesign Index page layout and update styles for hero and feature sections

--- frontend/web/src/components/Header/Header.js
+++ frontend/web/src/components/Header/Header.js
@@ -3,13 +3,38 @@ import { useNavigate } from 'react-router-dom';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useAuth } from '../../contexts/Auth';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  Typography
+} from '@mui/material';
+import { useState } from 'react';
 
 function Header() {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleNavigation = (path) => {
     navigate(path);
+  };
+
+  const handleLogoutClick = () => {
+    setOpenDialog(true);
+  };
+
+  const handleLogoutConfirm = () => {
+    logout();
+    handleNavigation('/login');
+    setOpenDialog(false);
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
   };
 
   return (
@@ -23,10 +48,7 @@ function Header() {
       {user ? (
         <LogoutIcon
           className={`${styles.icon} ${styles.logoutIcon}`}
-          onClick={() => {
-            logout();
-            handleNavigation('/login');
-          }}
+          onClick={handleLogoutClick}
         />
       ) : (
         <LoginIcon
@@ -34,6 +56,42 @@ function Header() {
           onClick={() => handleNavigation('/login')}
         />
       )}
+
+      <Dialog
+        open={openDialog}
+        onClose={handleDialogClose}
+        PaperProps={{
+          sx: {
+            backgroundColor: 'black',
+            color: 'white',
+            textAlign: 'center',
+            p: 3,
+            borderRadius: 2,
+            boxShadow: 'none',
+          }
+        }}
+      >
+        <DialogTitle>
+          <Typography variant="h6" sx={{ color: 'white' }}>
+            Confirmar Logout
+          </Typography>
+        </DialogTitle>
+
+        <DialogContent>
+          <Typography sx={{ color: 'white' }}>
+            Tem certeza que deseja sair?
+          </Typography>
+        </DialogContent>
+
+        <DialogActions sx={{ justifyContent: 'center', mt: 2 }}>
+          <Button onClick={handleDialogClose} sx={{ color: 'white' }}>
+            Cancelar
+          </Button>
+          <Button onClick={handleLogoutConfirm} sx={{ color: 'red' }}>
+            Sair
+          </Button>
+        </DialogActions>
+      </Dialog>
     </header>
   );
 }
