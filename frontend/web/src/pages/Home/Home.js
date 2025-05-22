76d5f59cd101d777dfbf0e2612314e22313f1b5c
Micael Rocha <micaelrrocha13@gmail.com>
2025-05-22 18:02:44 -0300

    feat: enhance Header and Home components with new icons and improved layout; update styles for better readability

--- frontend/web/src/pages/Home/Home.js
+++ frontend/web/src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import CreateIcon from '@mui/icons-material/Create';
 import BookIcon from '@mui/icons-material/Book';
-import PersonIcon from '@mui/icons-material/Person';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
 import Header from '../../components/Header/Header';
@@ -21,7 +20,18 @@ function Home() {
         <>
             <Header />
             <div className={styles.pageHome}>
-                <h1>Menu</h1>
+
+                <h1>SpeakUp</h1>
+
+                <h2>Aprenda e pratique inglês de uma inteligência artificial!</h2>
+
+                <p>O SpeakUp é um aplicativo de aprendizado de inglês que utiliza inteligência artificial para ajudar os usuários a praticar conversação e melhorar suas habilidades linguísticas. Com uma interface amigável e recursos interativos, o SpeakUp torna o aprendizado de inglês mais acessível e envolvente.</p>
+
+                <h3>Menu</h3>
+
+                <p>Escolha uma das opções abaixo para começar a praticar seu inglês!</p>
+
+                {/* Cards */}
                 <div className={styles.cardsContainer}>
                     <div className={styles.card} onClick={() => handleNavigate('/chat')}>
                         <CreateIcon sx={{ fontSize: '3rem' }} />
@@ -33,15 +43,11 @@ function Home() {
                         <h2>Plano de Ensino</h2>
                     </div>
 
-                    <div className={styles.card} onClick={() => handleNavigate('/palavreco')}>
+                    {/* <div className={styles.card} onClick={() => handleNavigate('/palavreco')}>
                         <AutoStoriesIcon sx={{ fontSize: '3rem' }} />
                         <h2>Palavreco</h2>
-                    </div>
+                    </div> */}
 
-                    <div className={styles.card} onClick={() => handleNavigate('/perfil')}>
-                        <PersonIcon sx={{ fontSize: '3rem' }} />
-                        <h2>Perfil</h2>
-                    </div>
                 </div>
             </div>
             <Footer />
